6964346b9241569a7c05220c1a456be825e4eed3
farhadabdillah51-eng <farhadabdillah51@gmail.com>
2026-06-21 02:53:55 +0700

    Update renderer.py

--- modules/renderer.py
+++ modules/renderer.py
@@ -1,1 +1,25 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
+def create_frame(
+    text,
+    width=1080,
+    height=1920
+):
+
+    img = Image.new(
+        "RGB",
+        (width, height),
+        (0, 0, 0)
+    )
+
+    draw = ImageDraw.Draw(img)
+
+    draw.text(
+        (100, 900),
+        text,
+        fill="white"
+    )
+
+    return img
