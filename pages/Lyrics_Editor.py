0803c7dbf1275eb80fa781f3fbe6149e7b6a19f7
farhadabdillah51-eng <farhadabdillah51@gmail.com>
2026-06-21 02:52:13 +0700

    Update Lyrics_Editor.py

--- pages/Lyrics_Editor.py
+++ pages/Lyrics_Editor.py
@@ -1,1 +1,32 @@
+import streamlit as st
+import pandas as pd
 
+st.title("📝 Editor Lirik")
+
+lyrics = st.text_area(
+    "Masukkan lirik",
+    height=300
+)
+
+if lyrics:
+
+    lines = lyrics.split("\n")
+
+    data = []
+
+    for i, line in enumerate(lines):
+
+        data.append({
+            "Lirik": line,
+            "Mulai": i * 3,
+            "Selesai": (i + 1) * 3
+        })
+
+    df = pd.DataFrame(data)
+
+    edited = st.data_editor(
+        df,
+        use_container_width=True
+    )
+
+    st.session_state["lyrics"] = edited
