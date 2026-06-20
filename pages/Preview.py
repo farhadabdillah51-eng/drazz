9a3e3535ba8b99c483a08045599f7ed7693ff385
farhadabdillah51-eng <farhadabdillah51@gmail.com>
2026-06-21 02:52:41 +0700

    Update Preview.py

--- pages/Preview.py
+++ pages/Preview.py
@@ -1,1 +1,20 @@
+import streamlit as st
 
+st.title("🎬 Preview")
+
+if "lyrics" not in st.session_state:
+    st.warning("Belum ada lirik")
+else:
+
+    df = st.session_state["lyrics"]
+
+    for _, row in df.iterrows():
+
+        st.markdown(
+            f"""
+            <h2 style='text-align:center'>
+            {row['Lirik']}
+            </h2>
+            """,
+            unsafe_allow_html=True
+        )
