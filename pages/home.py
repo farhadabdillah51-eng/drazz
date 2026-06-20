815e4cd9c6c12f7964690444010250cb32196f05
farhadabdillah51-eng <farhadabdillah51@gmail.com>
2026-06-21 02:51:39 +0700

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+st.title("🏠 Home")
+
+st.markdown("""
+### Selamat Datang
+
+Web ini digunakan untuk membuat video typography lirik.
+
+Fitur:
+- Upload Audio
+- Upload Video
+- Editor Timestamp
+- Preview
+- Export MP4
+""")
